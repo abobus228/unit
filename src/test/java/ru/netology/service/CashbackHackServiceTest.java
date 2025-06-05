9fd21ad5751cc1e84b8c6b1789c6135ac3576169
abobus228 <sashasafronov767@gmail.com>
2025-06-05 23:27:10 +0300

    Create tests on junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,38 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldReturnAmountForBonuses() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = service.remain(950);
+        int actual = 50;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturnAmountForBonusesAfter1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = service.remain(1150);
+        int actual = 850;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturnSum0At1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = service.remain(1000);
+        int actual = 0;
+
+        assertEquals(expected, actual);
+
     }
 }
