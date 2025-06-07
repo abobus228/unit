b46acace5a512cf273f7fda746af074fe196d990
abobus228 <sashasafronov767@gmail.com>
2025-06-07 16:07:12 +0300

    Change tests classes

--- src/test/java/ru/netology/service/CashbackHackServiceTestJunitJupiter.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJunitJupiter.java
@@ -1,43 +1,42 @@
 package ru.netology.service;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-public class CashbackHackServiceTest {
+public class CashbackHackServiceTestJunitJupiter {
 
     @Test
-    public void shouldReturnAmountForBonuses() {
+    public void shouldReturnAmountForBonusesJunitJupiter() {
 
         CashbackHackService service = new CashbackHackService();
 
         int expected = service.remain(950);
         int actual = 50;
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
     @Test
-    public void shouldReturnAmountForBonusesAfter1000() {
+    public void shouldReturnAmountForBonusesAfter1000JunitJupiter() {
 
         CashbackHackService service = new CashbackHackService();
 
         int expected = service.remain(1150);
         int actual = 850;
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
     @Test
-    public void shouldReturnSum0At1000() {
+    public void shouldReturnSum0At1000JunitJupiter() {
 
         CashbackHackService service = new CashbackHackService();
 
         int expected = service.remain(1000);
         int actual = 0;
 
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
-
 }
